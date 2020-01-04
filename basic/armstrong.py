8efa78703805787afc221cec6bb863d34d2f7d5e
amjad-hossain <amjadict07@gmail.com>
2020-01-04 15:02:02 +0800

    Check Armstrong Number

--- basic/armstrong.py
+++ basic/armstrong.py
@@ -1,17 +1,21 @@
-# Armstrong number:
-#
-# A number is called Armstrong number if it is equal to the sum of the cubes of its own digits.
-#
-# For example: 153 is an Armstrong number since 153 = 1*1*1 + 5*5*5 + 3*3*3.
-
 def armstrong(num, total):
     if num == 0:
         return total
 
     digit = num % 10
-    total += digit * digit * digit
+    total += digit ** 3
     return armstrong(num // 10, total)
 
 
 num = 153
-print(num == armstrong(num, 0))
+
+print('Is {} armstrong number?'.format(num), num == armstrong(num, 0))
+
+
+def find_armstrong_number(start, max):
+    for num in range(start, max + 1):
+        if num == armstrong(num, 0):
+            print('{} is armstrong'.format(num))
+
+
+find_armstrong_number(1, 5000)
