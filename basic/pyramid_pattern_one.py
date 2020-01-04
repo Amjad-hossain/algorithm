93f716ebfb72b63cd8e076f5e0c30fc29ba099a3
amjad-hossain <amjadict07@gmail.com>
2020-01-04 13:11:50 +0800

    Programs for Drawing Pyramid Pattern * * * * * * * * * * * * * * *

---
+++ basic/pyramid_pattern_one.py
@@ -0,0 +1,14 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+def pyramid(max_row):
+    for current_row in range(max_row):
+        for number_of_star in range(current_row + 1):
+            print('* ', end='')
+        print()
+
+
+pyramid(5)
