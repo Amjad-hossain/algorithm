011ae81d0acb136d81dd2a9e6075f9b5e2d3f5e0
amjad-hossain <amjadict07@gmail.com>
2020-08-05 15:12:04 +0800

    quick sort

--- d&c/quick_sort.py
+++ d&c/quick_sort.py
@@ -3,27 +3,56 @@
 # and all greater elements move to right side.
 # Finally, the algorithm recursively sorts the sub-arrays on left and right of pivot element.
 
-def quick_soft(a):
-    l = 0
-    al = len(a) - 1
-    r = al
-    while l < al:
-        while l < r:
-            if a[r - 1] > a[r]:
-                a[r - 1], a[r] = a[r], a[r - 1]
-            r = r - 1
-        r = al
-        l = l + 1
-    return a
+def quick_sort(dataset, first, last):
+    if first < last:
+        pivot_index = partition(dataset, first, last)
+        quick_sort(dataset, first, pivot_index - 1)
+        quick_sort(dataset, pivot_index + 1, last)
 
 
-a = [7, 2, 9, 3, 4, 5]
-print(quick_soft(a))
+def partition(dataset, first, last):
+    # Choose the first item
+    pivotvalue = dataset[first]
+    # establish upper and lower indexes
 
-a = [9, 2, 9, 3, 2, 8]
-print(quick_soft(a))
+    lower = first + 1
+    upper = last
+
+    # Start searching for crossing point
+    done = False
+    while not done:
+        # advance the lower index
+        while lower <= upper and dataset[lower] <= pivotvalue:
+            lower += 1
+        # decrease the upper index
+        while lower <= upper and pivotvalue <= dataset[upper]:
+            upper -= 1
+
+        # If the two indexws crossed, we got the split point
+        if upper < lower:
+            done = True
+        else:
+            # Exchange the values
+            temp = dataset[lower]
+            dataset[lower] = dataset[upper]
+            dataset[upper] = temp
+
+    # Exchange the pivot value
+    dataset[first] = dataset[upper]
+    dataset[upper] = pivotvalue
+
+    # return split index
+    return upper
+
+
+a = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+quick_sort(a, 0, len(a) - 1)
+print(a)
 
 a = [9, 8, 7, 6, 5, 4]
-print(quick_soft(a))
+quick_sort(a, 0, len(a) - 1)
+print(a)
+
 a = [5, 6, 7, 8, 9, 2]
-print(quick_soft(a))
+quick_sort(a, 0, len(a) - 1)
+print(a)
